25d5e88df2d9966ad5ef65b824584f9c338d2c9f
Dee Kay <dk@email.com>
2023-01-08 14:58:22 +0000

    updated contract address

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const TWITTER_HANDLE = 'It1swhat1t1s';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // Add the domain you will be minting
 const tld = '.strawberry';
-const CONTRACT_ADDRESS = 'YOUR_CONTRACT_ADDRESS_HERE';
+const CONTRACT_ADDRESS = '0x030E1D2e498827566A519C8d9DB5259270Ad7203';
 
 const App = () => {
 	//Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
@@ -61,6 +61,47 @@ const App = () => {
 	  }
 	};
 
+	const mintDomain = async () => {
+		// Don't run if the domain is empty
+		if (!domain) { return }
+		// Alert the user if the domain is too short
+		if (domain.length < 3) {
+			alert('Domain must be at least 3 characters long');
+			return;
+		}
+		// Calculate price based on length of domain (change this to match your contract)	
+		// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+		const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+		console.log("Minting domain", domain, "with price", price);
+		try {
+			const { ethereum } = window;
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+				console.log("Going to pop wallet now to pay gas...");
+				let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+				// Wait for the transaction to be mined
+				const receipt = await tx.wait();
+				// Check if the transaction was successfully completed
+				if (receipt.status === 1) {
+					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+					// Set the record for the domain
+					tx = await contract.setRecord(domain, record);
+					await tx.wait();
+					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+					setRecord('');
+					setDomain('');
+				}
+				else {
+					alert("Transaction failed! Please try again");
+				}
+			}
+		}
+		catch(error){
+			console.log(error);
+		}
+	}
 
 	// Create a function to render if wallet is not connected yet
 	const renderNotConnectedContainer = () => (
